437e60dbdfa03243011b72a2c71a5db27e148b92
leey00nsu <dbstndla1212@naver.com>
2023-05-25 22:06:26 +0900

    #1 소셜 계정 버튼 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -15,10 +15,11 @@ const LoginPage = () => {
   const onSubmit: SubmitHandler<any> = (data) => console.log(data);
   const onError: SubmitHandler<any> = (data) => console.log(data);
   return (
-    <div className="flex flex-col items-center w-screen">
+    <div className="flex flex-col items-center w-screen gap-10">
       <header className="flex justify-center text-6xl font-bold">
         Iceberg
       </header>
+      {/* 이메일로 로그인 */}
       <section className="flex justify-center w-full ">
         <form
           id="loginForm"
@@ -42,10 +43,27 @@ const LoginPage = () => {
             required
           />
 
-          <button type="submit" className="w-full btn btn-primary ">
+          <button type="submit" className="w-full btn btn-primary my-4 ">
             로그인
           </button>
         </form>
+      </section>
+
+      <section className="flex justify-between w-80 ">
+        <button className="bg-white border-gray-200 btn btn-lg btn-circle hover:border-gray-200 hover:bg-white ">
+          <img className="w-10 h-10 " src="/img/github_icon.png" />
+        </button>
+        <button className="bg-white border-gray-200 btn btn-lg btn-circle hover:border-gray-200 hover:bg-white">
+          <img src="/img/google_icon.png" />
+        </button>
+        <button className="bg-white border-0 btn btn-lg btn-circle hover:bg-white">
+          <img src="/img/naver_icon.png" />
+        </button>
+      </section>
+
+      <section className="flex justify-between w-80 items-center">
+        <p>아직 회원이 아니신가요?</p>
+        <button className="btn btn-ghost text-blue-500">회원가입</button>
       </section>
     </div>
   );
