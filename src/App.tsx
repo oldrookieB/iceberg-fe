a999002a119bfd522c06c3b31988125ec3980bbe
leey00nsu <dbstndla1212@naver.com>
2023-05-26 18:04:55 +0900

    #1 구글 OAuth 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/main" element={<MainPage />} />
     </Routes>
   );
 }
