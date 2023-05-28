e2cad801e0725306511ad74af9e33099c8945123
leey00nsu <dbstndla1212@naver.com>
2023-05-28 22:00:34 +0900

    #1 프로젝트 선택 페이지 구현

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ProjectCard = (props: any) => {
+  return (
+    <label className="h-40 border shadow-xl cursor-pointer shrink-0 card w-96 bg-base-100">
+      <div className="card-body">
+        <h2 className="card-title">{props.name}</h2>
+        <p>{props.description}</p>
+        <div className="justify-end card-actions">
+          <input
+            type="radio"
+            name="radio-project"
+            defaultChecked={props.index === 0 ? true : false}
+            className="w-8 h-8 checkbox checkbox-success"
+          />
+        </div>
+      </div>
+    </label>
+  );
+};
+
+export default ProjectCard;
