331bbec311f673f6172e13c417eb8dacf570a067
ovovvvvv <ovovvvvv@naver.com>
2023-06-17 01:53:04 +0900

    #8 메인페이지에 레포지토리 데이터 받음

--- src/components/list/Card.tsx
+++ src/components/list/Card.tsx
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-const Card = () => {
+interface CardProps {
+  title: string;
+  description: string;
+}
+
+
+const Card = (props: CardProps) => {
   const [isLiked, setIsLiked] = useState(false);
 
   const LikeClickHandler = () => {
@@ -10,25 +16,15 @@ const Card = () => {
   return (
     <div className="card card-compact w-80 h-64 bg-base-100 shadow-xl mx-4 mb-12">
       <figure>
-        <img src="src/assets/img/daom.jpg" alt="Img" className="object-cover" />
+        <img src="src/assets/img/daom.jpg" alt="Img" className='object-cover' />
       </figure>
       <div className="card-body flex h-24">
-        <h2 className="card-title">Cat !</h2>
-        <p>
-          What a lovely and cute cat! I hope all the cats in the universe are
-          happy!
-        </p>
+        <h2 className="card-title">{props.title}</h2>
+        <p>{props.description}</p>
       </div>
       <div className="card-actions justify-end mx-3 mb-1">
         <button className="" onClick={LikeClickHandler}>
-          <img
-            src={
-              isLiked
-                ? "src/assets/img/redheart.png"
-                : "src/assets/img/heart.png"
-            }
-            alt="Like"
-          />
+          <img src={isLiked ? "src/assets/img/redheart.png" : "src/assets/img/heart.png"} alt="Like" />
         </button>
       </div>
     </div>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,46 +1,89 @@
-import Layout from "../components/ui/Layout";
-import Header from "../components/ui/Header";
-import SearchBar from "../components/ui/SearchBar";
-import Card from "../components/list/Card";
-import TechButton from "../components/ui/TechButton";
-import { TECHSTACKS } from "../constants/techStacks";
+import axios from "axios";
+
+import React, { useState, useEffect } from 'react';
+import Layout from '../components/ui/Layout';
+import Header from '../components/ui/Header';
+import SearchBar from '../components/ui/SearchBar';
+import Card from '../components/list/Card';
+import TechButton from '../components/ui/TechButton';
+
+import { getRepositoryData } from '../api/github';
+import { Base64 } from "js-base64";
+import { useGithubAuthStore } from "../store/auth";
+import { Navigate } from "react-router-dom";
+import { useAuthStore } from "../store/auth";
+
 
 const MainPage = () => {
-  return (
-    <Layout>
-      <Header />
-      <div className="mt-12" />
-      <SearchBar />
-      <div className="mt-5" />
-      <a className="text-lg font-medium mx-5">Likes</a>
-      <div className="flex flex-wrap justify-start ml-2 mt-3">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
-      <a className="text-lg font-medium mx-5">TechStack</a>
-      <div className="flex flex-row flex-wrap mx-10 mt-3">
-        {TECHSTACKS.map((techstack, index) => (
-          <TechButton key={index} title={techstack.title} />
-        ))}
-      </div>
-      <div className="flex flex-wrap justify-start ml-2 mt-5">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    </Layout>
-  );
+  const [repositoryDatas, setRepositoryDatas] = useState([]);
+  const githubAuthStore = useGithubAuthStore();
+
+  useEffect(() => {
+    const fetchRepositoryData = async () => {
+      try {
+        const response = await getRepositoryData(githubAuthStore.userName);
+        setRepositoryDatas(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchRepositoryData();
+  },     []);
+  const TechStacks = [
+        { title: "React", bgColor: "#61dafb", txtColor: "black" },
+        { title: "Python", bgColor: "#3676AB", txtColor: "white" },
+        { title: "JavaScript", bgColor: "#f7df1e", txtColor: "black" },
+        { title: "html", bgColor: "#e34f26", txtColor: "white" },
+        { title: "Css", bgColor: "#264de4", txtColor: "white" },
+        { title: "node.js", bgColor: "#339933", txtColor: "white" },
+        { title: "java", bgColor: "#007396", txtColor: "white" },
+        { title: "ruby", bgColor: "#cc342d", txtColor: "white" },
+        { title: "angular", bgColor: "#dd0031", txtColor: "white" },
+        { title: "Vue.js", bgColor: "#4fc08d", txtColor: "white" },
+        { title: "c#", bgColor: "#68217a", txtColor: "white" }
+    ]
+    
+    return (
+       <Layout>
+            <Header/>
+            <div className='mt-12' />
+            <SearchBar/>
+            <div className='mt-5'/>
+            <a className='text-lg font-medium mx-5'>Likes</a>
+            <div className='flex flex-wrap justify-start ml-2 mt-3'>
+              {repositoryDatas.map((repository) => (
+                <Card
+                  key={repository.id}
+                  id={repository.id}  
+                  title={repository.name}
+                  description={repository.description}
+                />
+              ))}
+            </div>
+            <a className='text-lg font-medium mx-5'>TechStack</a>
+            <div className='flex flex-row flex-wrap mx-10 mt-3'>
+                {TechStacks.map((tech, index) => (
+                    <TechButton
+                        key={index}
+                        title={tech.title}
+                        bgColor={tech.bgColor}
+                        txtColor={tech.txtColor}
+                    />
+            ))}
+            </div>
+            <div className='flex flex-wrap justify-start ml-2 mt-5'>
+            {repositoryDatas.map((repository) => (
+                <Card
+                  key={repository.id}
+                  id={repository.id}  
+                  title={repository.name}
+                  description={repository.description}
+                />
+              ))}
+            </div>
+       </Layout>
+    );
 };
 
 export default MainPage;
