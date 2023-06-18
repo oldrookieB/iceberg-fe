acfc0fc4db9734fec54bec006c8471654379f5aa
ovovvvvv <ovovvvvv@naver.com>
2023-06-18 12:37:57 +0900

    #8 검색 기능 추가

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 
-const SearchBar = () => {
-    return (
-        <div className="form-control flex">
-        <div className="input-group justify-center">
-          <input type="text" placeholder="Search…" className="input input-bordered w-96" />
-          <button className="btn btn-square">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-          </button>
-        </div>
+interface SearchBarProps {
+  onSearch: (searchQuery: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchQuery);
+  };
+
+  return (
+    <div className='form-control flex'>
+      <div className='input-group justify-center'>
+        <input
+          type='text'
+          placeholder='Search...'
+          className='input input-bordered w-96'
+          value={searchQuery}
+          onChange={handleInputChange}
+        />
+        <button className='btn btn-square' onClick={handleSearch}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+            />
+          </svg>
+        </button>
       </div>
-    );
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 import React, { useState, useEffect } from 'react';
 import Layout from '../components/ui/Layout';
 import Header from '../components/ui/Header';
@@ -13,6 +11,7 @@ import { Link } from 'react-router-dom';
 
 const MainPage = () => {
   const [repositoryDatas, setRepositoryDatas] = useState([]);
+  const [searchResult, setSearchResult] = useState([]); //필터링 된 검색 결과 저장하기 위한 상태 설정
   const githubAuthStore = useGithubAuthStore();
 
   useEffect(() => {
@@ -26,28 +25,39 @@ const MainPage = () => {
     };
 
     fetchRepositoryData();
-  },     []);
+  }, []);
 
-    
-    return (
-       <Layout>
-            <Header/>
-            <div className='mt-12' />
-            <SearchBar/>
-            <div className='mt-5'/>
-     
-            <div className='flex flex-wrap justify-between' >
-              {repositoryDatas.map((repository: any) => (
-                <Link to={`/project/${repository.id}`} key = {repository.id}>
-                <Card
-                  title={repository.name}
-                  description={repository.description}
-                />
-                </Link>
-              ))}
-            </div>          
-       </Layout>
+  const handleSearch = (searchQuery: string) => {
+    const filteredRepositories = repositoryDatas.filter((repository) =>
+      repository.name.toLowerCase().includes(searchQuery.toLowerCase()) //검색어를 기준으로 레포지토리 필터링
     );
+    setSearchResult(filteredRepositories); //필터링 된 검색결과 설정
+  };
+
+  return (
+    <Layout>
+      <Header />
+      <div className='mt-12' />
+      <SearchBar onSearch={handleSearch} />
+      <div className='mt-5' />
+
+      <div className='flex flex-wrap justify-between'>
+        {searchResult.length > 0 ? (
+          searchResult.map((repository) => (
+            <Link to={`/project/${repository.id}`} key={repository.id}>
+              <Card title={repository.name} description={repository.description} />
+            </Link>
+          ))
+        ) : ( //검색결과가 없을 경우 모든 레포지토리 보여주기
+          repositoryDatas.map((repository) => (
+            <Link to={`/project/${repository.id}`} key={repository.id}>
+              <Card title={repository.name} description={repository.description} />
+            </Link>
+          ))
+        )}
+      </div>
+    </Layout>
+  );
 };
 
 export default MainPage;
